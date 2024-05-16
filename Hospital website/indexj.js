da7578f3181c0ea4f6d68e9676febdbea52f5c0f
Shashi Prabha <122176467+WednesdaySP@users.noreply.github.com>
2024-05-16 17:12:38 +0530

    Add files via upload

---
+++ Hospital website/indexj.js
@@ -0,0 +1,6 @@
+const slider = document.querySelector("input[type= 'checkbox']");
+const backgroundBody = document.querySelector('body'); 
+const switchBackground = document.querySelector('.switch');
+const navLinks = document.querySelectorAll('a');
+
+
