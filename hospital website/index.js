d84d64a3d1f81449796060d2565f2d2daab92855
Shashi Prabha <122176467+WednesdaySP@users.noreply.github.com>
2024-05-29 10:21:47 +0530

    Delete hospital website directory

--- hospital website/index.js
+++
@@ -1,7 +0,0 @@
-const slider = document.querySelector("input[type= 'checkbox']");
-const backgroundBody = document.querySelector('body'); 
-const switchBackground = document.querySelector('.switch');
-const navLinks = document.querySelectorAll('a');
-
-
-
